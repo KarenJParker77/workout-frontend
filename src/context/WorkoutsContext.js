5edd740323b3e833193101621763b7a3b133544c
KarenJParker77 <karen.mccrackle@gmail.com>
2023-02-16 17:35:16 +0000

    final frontend

---
+++ src/context/WorkoutsContext.js
@@ -0,0 +1,33 @@
+import { createContext, useReducer } from "react";
+
+export const WorkoutsContext = createContext();
+
+export const workoutsReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_WORKOUTS":
+      return {
+        workouts: action.payload,
+      };
+    case "CREATE_WORKOUT":
+      return {
+        workouts: [action.payload, ...state.workouts],
+      };
+    case "DELETE_WORKOUT":
+      return {
+        // filter method returns true or false. where not equal, keep in array, where equal, filter out
+        workouts: state.workouts.filter((w) => w._id !== action.payload._id),
+      };
+    default:
+      return state;
+  }
+};
+
+export const WorkoutsContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(workoutsReducer, { workouts: null });
+
+  return (
+    <WorkoutsContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </WorkoutsContext.Provider>
+  );
+};
